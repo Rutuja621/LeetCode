41a560becc16691011c5cf2c2d3584d709a73e2c
Rutuja Misal <rutujakmisal@gmail.com>
2026-04-17 17:49:19 +0530

    Time: 9 ms (7.17%), Space: 48.2 MB (6.36%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(j==nums[i]){
+                j++;
+
+            }
+
+        }
+        return j;
+
+
+    }
+}
