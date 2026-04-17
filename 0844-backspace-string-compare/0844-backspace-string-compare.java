bb50541476a2e6797e855f49f4252f0651a4dee6
Rutuja Misal <rutujakmisal@gmail.com>
2026-04-17 10:40:14 +0530

    Time: 3 ms (20.09%), Space: 43.3 MB (28.61%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+         return build(s).equals(build(t));
+    }
+
+     private String build(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : str.toCharArray()) {
+            if (c != '#') {
+                stack.push(c);
+            } else if (!stack.isEmpty()) {
+                stack.pop();
+            }
+        }
+        return String.valueOf(stack);
+    }
+}
