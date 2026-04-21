2e7870184fdf7e4e1eb6a8bba6b83baa2d2cdc92
Rutuja Misal <rutujakmisal@gmail.com>
2026-04-21 23:02:09 +0530

    Time: 0 ms (100%), Space: 41.7 MB (95.76%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+
+        int temp=x;
+        int sum=0;
+
+        while(temp > 0){
+            int digit=temp % 10;
+            sum += digit;
+            temp /= 10;
+        } 
+        
+        if(x % sum == 0){
+            return sum;
+
+        }
+        return -1;
+    }
+}
