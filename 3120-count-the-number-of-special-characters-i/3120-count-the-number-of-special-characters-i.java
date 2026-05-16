660813a55700af217afcf435d2d0ba1d715b6943
Rutuja Misal <rutujakmisal@gmail.com>
2026-05-16 16:44:05 +0530

    Time: 3 ms (32.43%), Space: 44 MB (33.01%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
@@ -1,14 +1,18 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
+          HashSet<Character> seen = new HashSet<>();
+        
+        // Add all characters to a set to remove duplicates
+        for (int i = 0; i < word.length(); i++) {
+            seen.add(word.charAt(i));
+        }
+        
         int specialCount = 0;
         
-        // Loop through all 26 lowercase alphabet letters
-        for (char ch = 'a'; ch <= 'z'; ch++) {
-            String lower = String.valueOf(ch);
-            String upper = String.valueOf(Character.toUpperCase(ch));
-            
-            // Check if both the lowercase and uppercase forms exist in the word
-            if (word.contains(lower) && word.contains(upper)) {
+        // Check every character in the set
+        for (char ch : seen) {
+            // If it's lowercase and its uppercase pair is also in the set
+            if (Character.isLowerCase(ch) && seen.contains(Character.toUpperCase(ch))) {
                 specialCount++;
             }
         }
