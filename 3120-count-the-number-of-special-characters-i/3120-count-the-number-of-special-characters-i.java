fee9e8fc7c14c7434c70271642b8a3b8837f9ade
Rutuja Misal <rutujakmisal@gmail.com>
2026-05-16 16:45:06 +0530

    Time: 1 ms (100%), Space: 43.7 MB (59.46%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
@@ -1,18 +1,23 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
-          HashSet<Character> seen = new HashSet<>();
+       boolean[] lower = new boolean[26];
+        boolean[] upper = new boolean[26];
         
-        // Add all characters to a set to remove duplicates
+        // Step 1: Record every character seen in the string
         for (int i = 0; i < word.length(); i++) {
-            seen.add(word.charAt(i));
+            char ch = word.charAt(i);
+            if (Character.isLowerCase(ch)) {
+                lower[ch - 'a'] = true;
+            } else if (Character.isUpperCase(ch)) {
+                upper[ch - 'A'] = true;
+            }
         }
         
         int specialCount = 0;
         
-        // Check every character in the set
-        for (char ch : seen) {
-            // If it's lowercase and its uppercase pair is also in the set
-            if (Character.isLowerCase(ch) && seen.contains(Character.toUpperCase(ch))) {
+        // Step 2: Count how many letters exist in both arrays
+        for (int i = 0; i < 26; i++) {
+            if (lower[i] && upper[i]) {
                 specialCount++;
             }
         }
