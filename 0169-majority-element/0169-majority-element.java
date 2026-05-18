cc118e15eef8f9276726607b0ce6152178276e55
Rutuja Misal <rutujakmisal@gmail.com>
2026-05-18 13:18:18 +0530

    Time: 1 ms (99.86%), Space: 55.5 MB (66.46%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,9 +1,29 @@
 class Solution {
     public int majorityElement(int[] nums) {
+
+
+        int candidate = nums[0];
+        int count = 1;
+        
+        // Phase 1: Identify the majority candidate
+        for (int i = 1; i < nums.length; i++) {
+            if (count == 0) {
+                candidate = nums[i];
+                count = 1;
+            } else if (nums[i] == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        
+        return candidate;
+
+        /*
         Arrays.sort(nums);
         int n = nums.length;
         return nums[n/2];
-        
+        */
       
         
     }
